b7a562a6dedd5fea76b39ac69d0e1bbbb850de83
Timur Sultanov <tssultanov@outlook.com>
2026-01-20 16:35:38 +0400

    scalars: prune unused init pipelines

--- src/solver/gpu/init/scalars.rs
+++ src/solver/gpu/init/scalars.rs
@@ -5,14 +5,9 @@ use crate::solver::model::KernelId;
 
 pub struct ScalarResources {
     pub bg_scalars: wgpu::BindGroup,
-    pub pipeline_init_scalars: wgpu::ComputePipeline,
     pub pipeline_init_cg_scalars: wgpu::ComputePipeline,
     pub pipeline_reduce_rho_new_r_r: wgpu::ComputePipeline,
     pub pipeline_reduce_r0_v: wgpu::ComputePipeline,
-    pub pipeline_reduce_t_s_t_t: wgpu::ComputePipeline,
-    pub pipeline_update_cg_alpha: wgpu::ComputePipeline,
-    pub pipeline_update_cg_beta: wgpu::ComputePipeline,
-    pub pipeline_update_rho_old: wgpu::ComputePipeline,
 }
 
 pub fn init_scalars(
@@ -22,11 +17,11 @@ pub fn init_scalars(
     b_dot_result_2: &wgpu::Buffer,
     b_solver_params: &wgpu::Buffer,
 ) -> ScalarResources {
-    let init_src = kernel_registry::kernel_source_by_id("", KernelId::SCALARS_INIT)
-        .unwrap_or_else(|e| panic!("missing scalars/init_scalars kernel: {e}"));
-    let pipeline_init_scalars = (init_src.create_pipeline)(device);
+    let init_cg_src = kernel_registry::kernel_source_by_id("", KernelId::SCALARS_INIT_CG)
+        .unwrap_or_else(|e| panic!("missing scalars/init_cg_scalars kernel: {e}"));
+    let pipeline_init_cg_scalars = (init_cg_src.create_pipeline)(device);
 
-    let bgl_scalars = pipeline_init_scalars.get_bind_group_layout(0);
+    let bgl_scalars = pipeline_init_cg_scalars.get_bind_group_layout(0);
     let registry = ResourceRegistry::new()
         .with_buffer("scalars", b_scalars)
         .with_buffer("dot_result_1", b_dot_result)
@@ -36,17 +31,12 @@ pub fn init_scalars(
         device,
         "Scalars Bind Group",
         &bgl_scalars,
-        init_src.bindings,
+        init_cg_src.bindings,
         0,
         |name| registry.resolve(name),
     )
     .unwrap_or_else(|err| panic!("failed to create scalars bind group: {err}"));
 
-    let pipeline_init_cg_scalars = {
-        let src = kernel_registry::kernel_source_by_id("", KernelId::SCALARS_INIT_CG)
-            .unwrap_or_else(|e| panic!("missing scalars/init_cg_scalars kernel: {e}"));
-        (src.create_pipeline)(device)
-    };
     let pipeline_reduce_rho_new_r_r = {
         let src = kernel_registry::kernel_source_by_id("", KernelId::SCALARS_REDUCE_RHO_NEW_R_R)
             .unwrap_or_else(|e| panic!("missing scalars/reduce_rho_new_r_r kernel: {e}"));
@@ -57,36 +47,11 @@ pub fn init_scalars(
             .unwrap_or_else(|e| panic!("missing scalars/reduce_r0_v kernel: {e}"));
         (src.create_pipeline)(device)
     };
-    let pipeline_reduce_t_s_t_t = {
-        let src = kernel_registry::kernel_source_by_id("", KernelId::SCALARS_REDUCE_T_S_T_T)
-            .unwrap_or_else(|e| panic!("missing scalars/reduce_t_s_t_t kernel: {e}"));
-        (src.create_pipeline)(device)
-    };
-    let pipeline_update_cg_alpha = {
-        let src = kernel_registry::kernel_source_by_id("", KernelId::SCALARS_UPDATE_CG_ALPHA)
-            .unwrap_or_else(|e| panic!("missing scalars/update_cg_alpha kernel: {e}"));
-        (src.create_pipeline)(device)
-    };
-    let pipeline_update_cg_beta = {
-        let src = kernel_registry::kernel_source_by_id("", KernelId::SCALARS_UPDATE_CG_BETA)
-            .unwrap_or_else(|e| panic!("missing scalars/update_cg_beta kernel: {e}"));
-        (src.create_pipeline)(device)
-    };
-    let pipeline_update_rho_old = {
-        let src = kernel_registry::kernel_source_by_id("", KernelId::SCALARS_UPDATE_RHO_OLD)
-            .unwrap_or_else(|e| panic!("missing scalars/update_rho_old kernel: {e}"));
-        (src.create_pipeline)(device)
-    };
 
     ScalarResources {
         bg_scalars,
-        pipeline_init_scalars,
         pipeline_init_cg_scalars,
         pipeline_reduce_rho_new_r_r,
         pipeline_reduce_r0_v,
-        pipeline_reduce_t_s_t_t,
-        pipeline_update_cg_alpha,
-        pipeline_update_cg_beta,
-        pipeline_update_rho_old,
     }
 }
